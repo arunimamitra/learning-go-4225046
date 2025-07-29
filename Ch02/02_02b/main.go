79141ec59122ab494e9055966827c0226b695012
Arunima Mitra <117327289+arunimamitra@users.noreply.github.com>
2025-07-29 04:47:27 +0000

    syntax1

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "John has"
+	str2 := "not finished"
+	str3 := "his homework"
 
-	fmt.Println("Hello from Go!")
+	// Whenever we call print functions, we get back
+	// two values - first is a numeric value which is the length of the string
+	// and second is the error object.
+	// We can receive these values by declaring the variables at the beginning of the statement
+
+	len, err := fmt.Println(str1, str2, str3)
+	fmt.Println("Error is", err)
+	fmt.Println("Length is", len)
 }
