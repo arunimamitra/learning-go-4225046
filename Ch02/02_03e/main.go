f191bf3b140c8ea78834ff247546d309f7bf14a6
Arunima Mitra <117327289+arunimamitra@users.noreply.github.com>
2025-07-29 05:04:37 +0000

    readingfromconsole

--- Ch02/02_03e/main.go
+++ Ch02/02_03e/main.go
@@ -9,12 +9,18 @@ import (
 )
 
 func main() {
+	// os.Stdin reads from console
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter text: ")
+	// In Go if we want to ignore an error, we use _ for it's name
+	// ReadString - function of the reader object which will read from console
+	// returns two values - string itself and the error
 	str, _ := reader.ReadString('\n')
 	fmt.Println(str)
 
 	fmt.Print("Enter a number: ")
+	// only use colon when declaring the variable (line 18)
+	// not required in line 24. only = will work
 	str, _ = reader.ReadString('\n')
 	f, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
 	if err != nil {
